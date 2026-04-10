8fcf530ed146462664a3da0e42b57a7f9a6dbf7a
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2026-04-10 06:23:37 +0600

    create user and booking api and table

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGODB_URI;
-const dbName = process.env.DB_NAME || "travelGuru";
-
-if (!uri) {
-  throw new Error("Missing MONGODB_URI. Add it to your .env file.");
-}
-
+const uri =
+  "mongodb+srv://travelGuru:o7StOuUYVwd4bkHK@cluster0.1ezipje.mongodb.net/?appName=Cluster0";
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -30,28 +24,178 @@ app.get("/", (req, res) => {
 async function run() {
   try {
     await client.connect();
-    const db = client.db(dbName);
-    const packageCollection = db.collection("package");
-
-    // All API is here
+    const db = client.db("travelGuru");
+    const packagesCollection = db.collection("packages");
+    const bookingCollection = db.collection("bookings");
+    const userCollection = db.collection("users");
+    // Define API endpoints for packages
     app.post("/packages", async (req, res) => {
-      const newPackage = req.body;
-      const result = await packageCollection.insertOne(newPackage);
+      const newPackage = { ...req.body, _id: new ObjectId() };
+      const result = await packagesCollection.insertOne(newPackage);
       res.send(result);
     });
 
     app.get("/packages", async (req, res) => {
-      const result = await packageCollection.find().toArray();
+      const cursor = packagesCollection.find({});
+      const packages = await cursor.toArray();
+      res.send(packages);
+    });
+
+    app.get("/packages/:id", async (req, res) => {
+      const id = req.params.id;
+
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "Invalid id" });
+      }
+
+      const result = await packagesCollection.findOne({
+        _id: new ObjectId(id),
+      });
       res.send(result);
     });
 
-    //avobe all re API
+    app.delete("/packages/:id", async (req, res) => {
+      const id = req.params.id;
+
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "Invalid id" });
+      }
+
+      const result = await packagesCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+    app.patch("/packages/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedPackage = req.body;
+
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "Invalid id" });
+      }
+
+      const result = await packagesCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedPackage },
+      );
+      res.send(result);
+    });
+
+    //api for booking
+    app.post("/bookings", async (req, res) => {
+      const newBooking = { ...req.body, _id: new ObjectId() };
+      const result = await bookingCollection.insertOne(newBooking);
+      res.send(result);
+    });
+
+    app.get("/bookings", async (req, res) => {
+      const cursor = bookingCollection.find({});
+      const bookings = await cursor.toArray();
+      res.send(bookings);
+    });
+
+    app.get("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "Invalid id" });
+      }
+
+      const result = await bookingCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "Invalid id" });
+      }
+
+      const result = await bookingCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+    app.patch("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedBooking = req.body;
+
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "Invalid id" });
+      }
+
+      const result = await bookingCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedBooking },
+      );
+      res.send(result);
+    });
+    //api for user
+    app.post("/users", async (req, res) => {
+      const newUser = { ...req.body, _id: new ObjectId() };
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find({});
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "Invalid id" });
+      }
+
+      const result = await userCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "Invalid id" });
+      }
+
+      const result = await userCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+    app.patch("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedUser = req.body;
+
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "Invalid id" });
+      }
+
+      const result = await userCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedUser },
+      );
+      res.send(result);
+    });
+
+    //End of API endpoints for packages
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
   } finally {
-    //await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
